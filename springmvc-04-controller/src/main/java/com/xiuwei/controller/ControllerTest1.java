c6ce66df723322c00d6c6afc1dba2ab7f9ecaf67
zhuxiuwei <jlu_zxw@126.com>
2021-09-09 09:06:37 +0800

    4 controller

---
+++ springmvc-04-controller/src/main/java/com/xiuwei/controller/ControllerTest1.java
@@ -0,0 +1,18 @@
+package com.xiuwei.controller;
+
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//实现Controller接口就是控制器了。
+public class ControllerTest1 implements Controller {
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("msg", "接口方式");    //添加数据
+        mv.setViewName("test1"); //设置视图名字--跳转
+        return null;
+    }
+}
---
+++ springmvc-04-controller/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zhuxingyu
+  Date: 2021/9/9
+  Time: 8:53 上午
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
