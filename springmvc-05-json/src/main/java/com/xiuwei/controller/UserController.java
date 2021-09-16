61e17a24f994c3709c2299dcb94276dcffa61add
zhuxiuwei <zhuxiuwei@zhuxiuweideMacBook-Pro.local>
2021-09-16 13:46:24 +0800

    jackson usage

--- springmvc-05-json/src/main/java/com/xiuwei/controller/UserController.java
+++ springmvc-05-json/src/main/java/com/xiuwei/controller/UserController.java
@@ -6,20 +6,37 @@ import com.xiuwei.pojo.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.HashSet;
+import java.util.Set;
+
+@RestController //@RestController，不走视图解析器了，会直接返回一个json
 public class UserController {
 
+    //单个对象
     @RequestMapping("/json1")
-    @ResponseBody   //加了ResponseBody注解，就不走视图解析器了，会直接返回一个字符串
-    public String json1() throws JsonProcessingException {
-        User user = new User("Michael Jackson", 18, "male");
+    //@ResponseBody   //加了ResponseBody注解，就不走视图解析器了，会直接返回一个字符串。(如果配置的不是@RestController，而是普通@Controller)
+    public String jacksonJson() throws JsonProcessingException {
+        User user = new User("王老二", 18, "男");
 
         //用Jackson，把对象转json
         ObjectMapper objectMapper = new ObjectMapper();
         String json = objectMapper.writeValueAsString(user);
 
 //        return user.toString(); //User{name='Michael Jackson', age=18, sex='male'}
-        return json;
+        return json;    //{"name":"Michael Jackson","age":18,"sex":"male"}
+    }
+
+    //集合对象
+    @RequestMapping("/json2")
+    //@ResponseBody   //加了ResponseBody注解，就不走视图解析器了，会直接返回一个字符串。(如果配置的不是@RestController，而是普通@Controller)
+    public String jacksonJson2() throws JsonProcessingException {
+        Set<User> users = new HashSet<User>();
+        users.add(new User("王老二", 18, "男"));
+        users.add(new User("唐大傻", 18, "女"));
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(users);
+        return json;    //[{"name":"唐大傻","age":18,"sex":"女"},{"name":"王老二","age":18,"sex":"男"}]
     }
 }
