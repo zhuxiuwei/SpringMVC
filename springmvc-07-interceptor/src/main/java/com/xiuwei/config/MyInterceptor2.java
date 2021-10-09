4e7eaebc7e31b28b12226e9f34726b12cbe309e2
zhuxiuwei <zhuxiuwei@zhuxiuweideMacBook-Pro.local>
2021-10-09 12:37:47 +0800

    ajax

--- springmvc-07-interceptor/src/main/java/com/xiuwei/config/MyInterceptor2.java
+++ springmvc-07-interceptor/src/main/java/com/xiuwei/config/MyInterceptor2.java
@@ -6,24 +6,22 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MyInterceptor implements HandlerInterceptor {
-
+public class MyInterceptor2 implements HandlerInterceptor {
 
     /**
-     * return true: 执行下一个拦截器/controller方法
-     * return false: 不执行下一个拦截器/controller方法 （执行到此为止，连controller都不进去了。）
-     * 高鹏设计模式，也提到了代理模式这个特性：代理类可以控制被代理类是否真正执行。
+     * 测试2个拦截器会咋样。
+     * 测试结果见有道云笔记。
      */
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("----------MyInterceptor执行前-----------");
+        System.out.println("----------MyInterceptor2执行前-----------");
         return true;
     }
 
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.println("----------MyInterceptor执行后-----------");
+        System.out.println("----------MyInterceptor2执行后-----------");
     }
 
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        System.out.println("----------MyInterceptor清理-----------");
+        System.out.println("----------MyInterceptor2清理-----------");
     }
 }
